5e1dd657fcfc0353c0288f3ec3f4441f771ed638
MANOJ Kumar ponnana <84012313+Manoj-kumar-ponnana@users.noreply.github.com>
2021-08-25 16:10:06 +0530

    Update DS_lab_exercise_6_by_47.c

--- DS_lab_exercise_6_by_47.c
+++ DS_lab_exercise_6_by_47.c
@@ -14,11 +14,6 @@ void main()
 	printf("enter the size of the stack :");
 	scanf("%d",&max);
 	int stack[max];
-	printf("\nenter the elements of stack:");
-	for(i=0;i<=max;i++)
-	{
-	scanf("%d",stack[i]);
-	}
 	do
 	{
 		printf("\n*****MAIN MENU*****");
